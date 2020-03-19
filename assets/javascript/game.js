7185e7c7336a401449a58e11e905389fba9f3c79
Firecraker <mezamic000@gmail.com>
2020-03-19 16:14:31 -0500

    added global variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,3 +13,12 @@ var selectableWords = [
 	"senate",
 	"congressional"
 ];
+
+var lettersString = "abcdefghijklmnopqrstuvwxyz".split("")[0];
+var maxGuesses = 8;
+var remainingGuesses = 0;
+var wins = 0;
+var guessedLetters = [];
+var currentWord = [];
+var currentWordIndex;
+var wordBlanks;
