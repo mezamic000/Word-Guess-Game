23cb369890107264657750d063b40e0b8fbb87f3
Firecraker <mezamic000@gmail.com>
2020-03-19 15:56:29 -0500

    created a word array that player will be guessing

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var selectableWords = [
+	"legislature",
+	"democratic",
+	"american",
+	"constitution",
+	"states",
+	"presidents",
+	"freedom",
+	"Independence",
+	"liberty",
+	"patriotic",
+	"washington",
+	"senate",
+	"congressional"
+];
