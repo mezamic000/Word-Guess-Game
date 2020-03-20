a3a5e3bf09586e40d0a5774b2c095fc5dacaac5e
Firecraker <mezamic000@gmail.com>
2020-03-20 10:39:06 -0500

    added keydown event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,14 +22,34 @@ var guessedLetters = [];
 var currentWord = [];
 var currentWordIndex;
 var wordBlanks = [];
+var playersGuess;
 
 function getCurrentWord() {
-	selectableWords[Math.floor(Math.random() * selectableWords.length)];
+	currentWord =
+		selectableWords[Math.floor(Math.random() * selectableWords.length)];
 	console.log(selectableWords);
 }
 
 function displayBlanks() {
 	for (let i = 0; i < getCurrentWord.length; i++) {
 		wordBlanks.push("_");
+		document.write.getElementById("currentWord");
 	}
 }
+
+function startGame() {
+	getCurrentWord();
+	displayBlanks();
+}
+startGame();
+
+document.onkeydown = function(event) {
+	var playersGuess = event.key.toLowerCase();
+	console.log(playersGuess);
+	if (lettersString.includes(playersGuess) == false) {
+		console.log("Invalid entry");
+	} else if (guessedLetters.includes(playersGuess)) {
+		console.log("That letter has been selected");
+	} else {
+	}
+};
