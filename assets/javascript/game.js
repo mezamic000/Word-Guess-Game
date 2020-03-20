0ae2478984d915ecbc6a1aa78ec9a41645350bcb
Firecraker <mezamic000@gmail.com>
2020-03-19 20:51:01 -0500

    function to select random word, display blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,11 +14,22 @@ var selectableWords = [
 	"congressional"
 ];
 
-var lettersString = "abcdefghijklmnopqrstuvwxyz".split("")[0];
+var lettersString = "abcdefghijklmnopqrstuvwxyz".split("");
 var maxGuesses = 8;
 var remainingGuesses = 0;
 var wins = 0;
 var guessedLetters = [];
 var currentWord = [];
 var currentWordIndex;
-var wordBlanks;
+var wordBlanks = [];
+
+function getCurrentWord() {
+	selectableWords[Math.floor(Math.random() * selectableWords.length)];
+	console.log(selectableWords);
+}
+
+function displayBlanks() {
+	for (let i = 0; i < getCurrentWord.length; i++) {
+		wordBlanks.push("_");
+	}
+}
